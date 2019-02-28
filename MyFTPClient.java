8a990b703f2c9ce89cdb26e98ab1b3558c4db20c
Mashyanov <46908470+Mashyanov@users.noreply.github.com>
2019-02-28 14:34:04 +0300

    Update MyFTPClient.java

--- MyFTPClient.java
+++ MyFTPClient.java
@@ -1,4 +1,3 @@
-
 package CrazyClients;
 
 import java.io.File;
@@ -6,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -16,42 +16,38 @@ import org.apache.commons.net.ftp.FTPClient;
  *
  * @author Александр Машьянов, mashyanov1987@gmail.com
  */
-public class MyFTPClient {
+public final class MyFTPClient {
  
-private FileInputStream fis;
-private FTPClient client;
+    private FileInputStream fis;
+    private FTPClient client;
+    private final Properties prop = new Properties();
 
-//АКТУАЛЬНЫЙ СЕРВЕР
-
-private final String FTPlogin = ":)";
-private final String FTPpassword = ":)";
-private final String FTPhost = ":)";
-private static MyFTPClient myFTPClient = null;
+    private static MyFTPClient myFTPClient = null;
 
     private MyFTPClient() {
         client = new FTPClient();
-    try {
-       
-        Runtime p = Runtime.getRuntime();
-        p.exec("netsh advfirewall set global StatefulFTP disable");
-    } catch (IOException ex) {
-        Logger.getLogger(MyFTPClient.class.getName()).log(Level.SEVERE, null, ex);
-    }
+        try {
+            prop.load(new FileInputStream("DBProperties.properties"));
+            Runtime p = Runtime.getRuntime();
+            p.exec("netsh advfirewall set global StatefulFTP disable");
+        } catch (IOException ex) {
+            Logger.getLogger(MyFTPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public static MyFTPClient getInstance(){
-    if(myFTPClient == null) myFTPClient = new MyFTPClient();
-    return myFTPClient;
- }
+        if(myFTPClient == null) myFTPClient = new MyFTPClient();
+        return myFTPClient;
+    }
 
     public int connect(){
         try {
-            
             System.out.println("Подключаемся к FTP-серверу...");
-            client.connect(FTPhost);
+            client.connect(prop.getProperty("FTPhost"));
             if(!client.isConnected()) return 11;
             client.enterLocalPassiveMode();
-            boolean logged = client.login(FTPlogin, FTPpassword);
+            boolean logged = client.login(prop.getProperty("FTPlogin"), 
+                                          prop.getProperty("FTPpassword"));
             if(logged) {
                 System.out.println("Успешное подключение!");
             return 0;
@@ -69,36 +65,33 @@ private static MyFTPClient myFTPClient = null;
     return 22;
 }
     public void disconnect(){
-    try {
-        client.disconnect();
-    } catch (IOException ex) {
-        Logger.getLogger(MyFTPClient.class.getName()).log(Level.SEVERE, null, ex);
+        try {
+            client.disconnect();
+        } catch (IOException ex) {
+            Logger.getLogger(MyFTPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    }
-public boolean uploadDBBackUp() {
-boolean result = false; 
-    LocalDateTime date = LocalDateTime.now();
-    File dir = new File("backup/cc.bak");
     
-    try {
-        fis = new FileInputStream(dir);
-        client.changeWorkingDirectory(":)");
-        client.setFileType(FTP.BINARY_FILE_TYPE);
-        String fileName = date.format(DateTimeFormatter.ofPattern("dd-MM-uuuu(HH-mm-ss)"));
-        String files[] = client.listNames();
-        System.out.println(files.length);
-        if(files.length > 9)
-            client.deleteFile(files[2]);
-        result = client.appendFile(fileName+".bak", fis);
-        fis.close();
-        System.out.println(date.toString());
-        System.out.println(result);
-        return result;
-    } catch (IOException ex) {
-        System.out.println("IO error " + ex.getMessage());
-    }
-    return result;
-}
+    public boolean uploadDBBackUp() {
+        boolean result = false; 
+        LocalDateTime date = LocalDateTime.now();
+        File dir = new File("backup/cc.bak");
 
+        try {
+            fis = new FileInputStream(dir);
+            client.setFileType(FTP.BINARY_FILE_TYPE);
+            client.changeWorkingDirectory(prop.getProperty("FTPWorkDirectory"));
+            String fileName = date.format(DateTimeFormatter.ofPattern("dd-MM-uuuu(HH-mm-ss)"))+"devPC";
+            String files[] = client.listNames();
+            if(files.length > 9)
+                client.deleteFile(files[2]);
+            result = client.appendFile(fileName+".bak", fis);
+            fis.close();
+            return result;
+        } catch (IOException ex) {
+            System.out.println("IO error " + ex.getMessage());
+        }
+        return result;
+    }
 
 }
